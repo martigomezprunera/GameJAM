868506a528bc270851e674b4e87a45e415998496
Ignacio <ignacioperezvelasco12@gmail.com>
2020-09-22 19:19:18 +0200

    Starting Organitzation

--- New Unity Project/Assets/Scripts/GameManager.cs
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum actions { ATACAR, ATACARFUERTE1, ATACARFUERTE2, PARRY1, PARRY2, ESQUIVAR, EXHAUST };
+
 public class GameManager : MonoBehaviour
 {
     #region VARIABLES
@@ -13,6 +15,8 @@ public class GameManager : MonoBehaviour
     private float countDownRound;
     private bool selectingActions;
     private float roundDuration;
+    private List<actions> iaActions;
+    private int aux;
     #endregion
 
     #region METHODS
@@ -22,10 +26,11 @@ public class GameManager : MonoBehaviour
         roundDuration = 3f;
         countDownRound = roundDuration;
         numRound = 1;
+        aux = 0;
         selectingActions = true;
     }
 
-    void resetCountDownRound()
+    void ResetCountDownRound()
     {
         countDownRound = roundDuration;
     }
@@ -40,23 +45,204 @@ public class GameManager : MonoBehaviour
                 Debug.Log(countDownRound);
             }
             else
+            {
+                if (myPlayer.myActions.Count == 0)
+                {
+                    for (int i = 0; i < numRound; i++)
+                    {
+                        myPlayer.myActions.Add(actions.EXHAUST);
+                    }
+                }
+                //doAnimations();
                 CompareActions();
+            }                
         }
+    }
+
+    void doAnimations()
+    {        
+        myPlayer.DoAction(myPlayer.myActions[aux]);
+        //activar animacion de ia
     }
 
     void CompareActions()
     {
-        Debug.Log("Se comparan las acciones");
-        resetCountDownRound();
+        /*if (aux < numRound)
+        {
+            switch (myPlayer.myActions[aux])
+            {
+                case actions.ATACAR:
+                    switch (iaActions[aux])
+                    {
+                        case actions.ATACAR:
+                            myPlayer.getDamage(10);
+                            break;
+                        case actions.ATACARFUERTE1:
+                            //agent get light damage
+                            break;
+                        case actions.ATACARFUERTE2:
+                            myPlayer.getDamage(20);
+                            //agent get light damage
+                            break;
+                        case actions.PARRY1:
+                            myPlayer.getDamage(10);
+                            break;
+                        case actions.PARRY2:
+                            //agent get light damage
+                            break;
+                        case actions.ESQUIVAR:
+                            //nothing
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case actions.ATACARFUERTE1:
+                    switch (iaActions[aux])
+                    {
+                        case actions.ATACAR:
+                            myPlayer.getDamage(10);
+                            break;
+                        case actions.ATACARFUERTE1:
+                            //nada
+                            break;
+                        case actions.ATACARFUERTE2:
+                            myPlayer.getDamage(20);
+                            //Cortar el siguiente movimiento de myplayer
+                            break;
+                        case actions.PARRY1:
+                            //Nothing
+                            break;
+                        case actions.PARRY2:
+                            //Nothing
+                            break;
+                        case actions.ESQUIVAR:
+                            //Nothing
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case actions.ATACARFUERTE2:
+                    switch (iaActions[aux])
+                    {
+                        case actions.ATACAR:
+                            myPlayer.getDamage(10);
+                            //agent get heavy damage
+                            break;
+                        case actions.ATACARFUERTE1:
+                            //agent get heavy damage y cortar su siguiente movimiento
+                            break;
+                        case actions.ATACARFUERTE2:
+                            myPlayer.getDamage(20);
+                            //agent get heavy damage
+                            break;
+                        case actions.PARRY1:
+                            myPlayer.getDamage(10);
+                            break;
+                        case actions.PARRY2:
+                            //agent get heavy damage
+                            break;
+                        case actions.ESQUIVAR:
+                            //nothing
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case actions.PARRY1:
+                    switch (iaActions[aux])
+                    {
+                        case actions.ATACAR:
+                            //agent get light damage
+                            break;
+                        case actions.ATACARFUERTE1:
+                            //nada
+                            break;
+                        case actions.ATACARFUERTE2:
+                            //agent get light damage
+                            break;
+                        case actions.PARRY1:
+                            //nada
+                            break;
+                        case actions.PARRY2:
+                            //nada
+                            break;
+                        case actions.ESQUIVAR:
+                            //nada
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case actions.PARRY2:
+                    switch (iaActions[aux])
+                    {
+                        case actions.ATACAR:
+                            myPlayer.getDamage(10);
+                            break;
+                        case actions.ATACARFUERTE1:
+                            //nothing
+                            break;
+                        case actions.ATACARFUERTE2:
+                            myPlayer.getDamage(20);
+                            break;
+                        case actions.PARRY1:
+                            //nothing
+                            break;
+                        case actions.PARRY2:
+                            //nothing
+                            break;
+                        case actions.ESQUIVAR:
+                            //nothing
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case actions.ESQUIVAR:
+                    switch (iaActions[aux])
+                    {
+                        case actions.ATACAR:
+                            break;
+                        case actions.ATACARFUERTE1:
+                            break;
+                        case actions.ATACARFUERTE2:
+                            break;
+                        case actions.PARRY1:
+                            break;
+                        case actions.PARRY2:
+                            break;
+                        case actions.ESQUIVAR:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            aux++;
+            doAnimations();
+        }
+        else*/
+            FinishRound();
+
+    }
+
+    void FinishRound()
+    {
+        ResetCountDownRound();
         selectingActions = false;
         myPlayer.ClearActions();
         numRound++;
-        // do animations and when finish activate selectingActions
     }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         HandleRound(selectingActions);
+
         if (Input.GetKeyDown("space"))
         {
             selectingActions = true; 
--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     #region VARIABLES
-    enum actions {ATACAR, ATACARFUERTE, PARRY, ESQUIVAR};
 
-    [SerializeField] List<actions>  myActions;
+    public List<actions> myActions;
     GameManager myGameManager;
+    private int life;
     #endregion
 
     #region METHODS
     private void Start()
     {
-        myGameManager= GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        myGameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        life = 100;
     }
 
     public void ClearActions()
@@ -25,17 +26,53 @@ public class Player : MonoBehaviour
     private void Update()
     {
         //ataque
-        if ( (Input.GetKeyDown("1")) && (myActions.Count<myGameManager.numRound))
+        if ((Input.GetKeyDown("1")) && (myActions.Count < myGameManager.numRound))
             myActions.Add(actions.ATACAR);
         //AtaqueFuerte
-        else if ((Input.GetKeyDown("2")) && (myActions.Count < myGameManager.numRound))
-            myActions.Add(actions.ATACARFUERTE);
+        else if ((Input.GetKeyDown("2")) && ((myActions.Count+1) < myGameManager.numRound))
+        {
+            myActions.Add(actions.ATACARFUERTE1);
+            myActions.Add(actions.ATACARFUERTE2);
+        }
         //Parry
         else if ((Input.GetKeyDown("3")) && (myActions.Count < myGameManager.numRound))
-            myActions.Add(actions.ESQUIVAR);
+            myActions.Add(actions.PARRY1);
         //Esquivar
         else if ((Input.GetKeyDown("4")) && (myActions.Count < myGameManager.numRound))
-            myActions.Add(actions.PARRY);
+            myActions.Add(actions.ESQUIVAR);
+    }
+
+    public void DoAction(actions toDO)
+    {
+        //Se activa la animacion que toque
+        switch (toDO)
+        {
+            case actions.ATACAR:
+                //activar animacion
+                break;
+            case actions.ATACARFUERTE1:
+                //activar animacion
+                break;
+            case actions.ATACARFUERTE2:
+                //activar animacion
+                break;
+            case actions.PARRY1:
+                //activar animacion
+                break;
+            case actions.PARRY2:
+                //activar animacion
+                break;
+            case actions.ESQUIVAR:
+                //activar animacion
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void getDamage(int damage)
+    {
+        life -= damage;
     }
     #endregion
 }
