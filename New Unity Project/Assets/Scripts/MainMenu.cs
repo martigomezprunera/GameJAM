65d632acc5d567b884621a70edd3e6167ff1a1ea
Martí <magopru1995@gmail.com>
2020-09-23 01:44:28 +0200

    MenuLogic + ButtonsHovers

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //HAY QUE AÑADIR LA ESCENA AL BUILDEAR
+        Debug.Log("Empecemos");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Debo cerrarme");
+        Application.Quit();
+    }
+}
