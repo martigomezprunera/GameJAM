d8b0e9f41bd487ea4bc2817ede677cda2b0df229
Ignacio <ignacioperezvelasco12@gmail.com>
2020-09-22 18:11:02 +0200

    Gestionar tiempo rondas, num acciones x ronda y escoger acciones

---
+++ New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region VARIABLES
+    //public//////////////////////
+    public int numRound;
+    public Player myPlayer;
+
+    //Private//////////////////////
+    private float countDownRound;
+    private bool selectingActions;
+    private float roundDuration;
+    #endregion
+
+    #region METHODS
+    void Start()
+    {
+        //Initialize
+        roundDuration = 3f;
+        countDownRound = roundDuration;
+        numRound = 1;
+        selectingActions = true;
+    }
+
+    void resetCountDownRound()
+    {
+        countDownRound = roundDuration;
+    }
+
+    void HandleRound(bool selecting)
+    {
+        if (selecting)
+        {
+            if (countDownRound > 0f)
+            {
+                countDownRound -= Time.deltaTime;
+                Debug.Log(countDownRound);
+            }
+            else
+                CompareActions();
+        }
+    }
+
+    void CompareActions()
+    {
+        Debug.Log("Se comparan las acciones");
+        resetCountDownRound();
+        selectingActions = false;
+        myPlayer.ClearActions();
+        numRound++;
+        // do animations and when finish activate selectingActions
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleRound(selectingActions);
+        if (Input.GetKeyDown("space"))
+        {
+            selectingActions = true; 
+        }
+    }
+    #endregion
+}
