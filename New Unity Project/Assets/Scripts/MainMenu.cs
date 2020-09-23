d172c6c7d689f3d970b78f8a2647dd2266a4fd11
Martí <magopru1995@gmail.com>
2020-09-23 15:18:53 +0200

    PauseMenu + MainMenu FINISHED

--- New Unity Project/Assets/Scripts/MainMenu.cs
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour
     {
         //HAY QUE AÑADIR LA ESCENA AL BUILDEAR
         Debug.Log("Empecemos");
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitGame()
