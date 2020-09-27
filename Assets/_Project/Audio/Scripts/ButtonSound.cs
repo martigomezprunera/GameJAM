23262230f1c29de4d9006911265094d4d1d0f66c
Martí <magopru1995@gmail.com>
2020-09-27 19:23:18 +0200

    Credits / Como se juega

--- Assets/_Project/Audio/Scripts/ButtonSound.cs
+++ Assets/_Project/Audio/Scripts/ButtonSound.cs
@@ -12,6 +12,6 @@ public class ButtonSound : MonoBehaviour, IPointerEnterHandler, IPointerDownHand
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        AudioManager.Instance.PlaySound(HoverSound);
+        AudioManager.Instance.PlaySound(PressSound);
     }
 }
